169fa404ea1ac4b8a9c6265f517a1eb8ae0b705b
heweiming <heweiming89@163.com>
2016-04-24 11:36:37 +0800

    2016年4月24日 11:36:34

---
+++ src/test/java/cn/heweiming/httpclient/demo01/HttpClientDemo01.java
@@ -0,0 +1,40 @@
+package cn.heweiming.httpclient.demo01;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.junit.Test;
+
+public class HttpClientDemo01 {
+
+	@Test
+	public void testGet() throws HttpException, IOException {
+		HttpClient client = new HttpClient();
+		String host = "http://ws.webxml.com.cn";
+		String getStr = "/WebServices/MobileCodeWS.asmx/getMobileCodeInfo?mobileCode=15800979640&userID=";
+		GetMethod get = new GetMethod(host + getStr);
+		int i = client.executeMethod(get);
+		System.out.println(i);
+		String string = get.getResponseBodyAsString();
+		System.out.println(string);
+
+	}
+
+	@Test
+	public void testPost() throws HttpException, IOException {
+		HttpClient client = new HttpClient();
+		String url = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx/getMobileCodeInfo";
+		PostMethod post = new PostMethod(url);
+		post.setParameter("mobileCode", "18973579870");
+		post.setParameter("userID", "");
+		if (client.executeMethod(post) == 200) {
+			String responseStr = post.getResponseBodyAsString();
+			System.out.println(responseStr);
+		}
+
+	}
+
+}

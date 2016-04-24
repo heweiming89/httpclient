16f5527b9f58280f89bbc2ef678cd575a07e563c
heweiming <heweiming89@163.com>
2016-04-24 14:21:56 +0800

    2016年4月24日 14:21:52

--- src/test/java/cn/heweiming/httpclient/demo01/HttpClientDemo01.java
+++ src/test/java/cn/heweiming/httpclient/demo01/HttpClientDemo01.java
@@ -1,6 +1,7 @@
 package cn.heweiming.httpclient.demo01;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
@@ -8,6 +9,7 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.junit.Test;
 
+@SuppressWarnings("deprecation")
 public class HttpClientDemo01 {
 
 	@Test
@@ -28,7 +30,8 @@ public class HttpClientDemo01 {
 		HttpClient client = new HttpClient();
 		String url = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx/getMobileCodeInfo";
 		PostMethod post = new PostMethod(url);
-		post.setParameter("mobileCode", "18973579870");
+		String mobileCode = "18973579870";
+		post.setParameter("mobileCode", mobileCode);
 		post.setParameter("userID", "");
 		if (client.executeMethod(post) == 200) {
 			String responseStr = post.getResponseBodyAsString();
@@ -37,8 +40,38 @@ public class HttpClientDemo01 {
 	}
 
 	@Test
-	public void testSoap() {
-		
+	public void testSoap1_1() throws HttpException, IOException {
+		HttpClient client = new HttpClient();
+		String url = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx";
+		PostMethod post = new PostMethod(url);
+		InputStream input = this.getClass().getClassLoader()
+				.getResourceAsStream("soap/getMobileCodeInfo_soap1.1.xml");
+		post.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+		post.setRequestBody(input);
+		int i = client.executeMethod(post);
+		System.out.println(i);
+		if (i == 200) {
+			String responseStr = post.getResponseBodyAsString();
+			System.out.println(responseStr);
+		}
+	}
+
+	@Test
+	public void testSoap1_2() throws HttpException, IOException {
+		HttpClient client = new HttpClient();
+		String url = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx";
+		PostMethod post = new PostMethod(url);
+		InputStream input = this.getClass().getClassLoader()
+				.getResourceAsStream("soap/getMobileCodeInfo_soap1.2.xml");
+		post.setRequestHeader("Content-Type",
+				"application/soap+xml; charset=utf-8");
+		post.setRequestBody(input);
+		int i = client.executeMethod(post);
+		System.out.println(i);
+		if (i == 200) {
+			String responseStr = post.getResponseBodyAsString();
+			System.out.println(responseStr);
+		}
 	}
 
 }
